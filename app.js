0c9a6a0fec306c7ccca2096af85e74d508dbdf73
Ishyrich88 <167790636+Ishyrich88@users.noreply.github.com>
2024-06-18 11:44:37 -0400

    created a function to check is number is prime

--- app.js
+++ app.js
@@ -11,3 +11,20 @@ for (let i = 0; i < 100; i++) {
         console.log (i);
     }
 }
+
+console.log(5)
+
+// Part 2
+
+// Function to check if a number is prime
+function isPrime(num) {
+    if (num <= 1) return false; 
+    if (num <= 3) return true; 
+
+    if (num % 2 === 0 || num % 3 === 0) return false; 
+
+    for (let i = 5; i * i <= num; i += 6) {
+        if (num % i === 0 || num % (i + 2) === 0) return false; 
+    }
+    return true; 
+}
